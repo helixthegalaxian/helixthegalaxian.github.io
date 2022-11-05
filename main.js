20dcc6317d3e4b1fed0b60414180d03f023f7db7
helixthegalaxian <111297917+helixthegalaxian@users.noreply.github.com>
2022-11-05 14:08:31 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log("hello")
+
+console.warn(hellooo)
+console.error(error);
+
+await AudioContext.sync();
+console.log(`The range address was ${range.address}.`);
+
+await Excel.run(async context => {/** 
+* Insert your Excel code here
+*/})
